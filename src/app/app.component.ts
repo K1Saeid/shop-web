dd2da29d6e691a5cfac39812647a153fc9012466
Saeid <saeid.a.hal@gmail.com>
2025-11-03 12:58:10 +0100

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from "./top-header/top-header.component";
+import { ContainerComponent } from "./container/container.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, TopHeaderComponent, ContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'shop-web';
 }
+
